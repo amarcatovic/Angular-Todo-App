64a14f79be2aff5839193fb6d01c81e8091c0bc6
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-14 16:57:06 +0200

    add props to constructor

--- src/app/models/Todo.ts
+++ src/app/models/Todo.ts
@@ -1,11 +1,5 @@
 export default class Todo{
-    id: number;
-    title: string;
-    description: string;
-    done: boolean;
-    dateCreated: string;
-
-    constructor() {
+    constructor(public id: number, public title: string, public description: string, public done: boolean, public dateCreated: string) {
         this.dateCreated = new Date().toLocaleString();
     }
 }
