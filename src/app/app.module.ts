3cf2575dea524e5008b84967b87eace80caa4a9b
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-14 15:53:37 +0200

    add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoItemComponent,
-    TodoListComponent
+    TodoListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
