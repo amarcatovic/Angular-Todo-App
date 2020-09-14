4773aea5c22c87509d55b70f6341fde884d5ddfa
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-14 18:15:20 +0200

    add todos array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Todo from './models/Todo';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent { 
   
+  todos: Todo[] = [new Todo(1, 'Title', 'Description', false, new Date().toDateString())];
   viewName: string = 'todo-list';
   
   selectTodoView(viewName){
       this.viewName = viewName;
+  }
+
+  addTodoToList(todo){
+    this.todos = [...this.todos, todo];
   }
 }
