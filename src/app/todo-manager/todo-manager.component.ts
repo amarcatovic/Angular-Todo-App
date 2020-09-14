efdf3231aec18c813489321599d4dc9a42e8ceef
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-14 18:16:15 +0200

    implement form logic

--- src/app/todo-manager/todo-manager.component.ts
+++ src/app/todo-manager/todo-manager.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import Todo from '../models/Todo';
 
 @Component({
   selector: 'app-todo-manager',
   templateUrl: './todo-manager.component.html',
   styleUrls: ['./todo-manager.component.css']
 })
-export class TodoManagerComponent implements OnInit {
+export class TodoManagerComponent {
+  
+  title: string;
+  description: string;
 
-  constructor() { }
+  todoError: boolean = false;
+  newTodoAdded: boolean = false;
 
-  ngOnInit(): void {
+  @Output() todoAdded = new EventEmitter<Todo>();
+
+  onCreateTodoButtonClick(){
+    if(this.title === '' || this.description === '')
+    {
+      this.todoError = true;
+    }
+    else
+    {
+      const newTodo: Todo = new Todo(1, this.title, this.description, false, new Date().toLocaleString());
+      this.todoAdded.emit(newTodo);
+      this.onClearButtonClicked();
+      this.newTodoAdded = true;
+      setTimeout(_ => this.newTodoAdded = false, 3000);
+    }
+  }
+
+  onClearButtonClicked(){
+    this.title = '';
+    this.description = '';
+    this.todoError = false;
   }
 
 }
