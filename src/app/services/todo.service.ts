eabf8b4d3ab5d00323752f10f41b3be84880a941
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-15 17:05:02 +0200

    implement todo service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import Todo from '../models/Todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  todoScreen = new EventEmitter<string>();
+
+  todos: Todo[] = [];
+
+  constructor() { 
+    this.todos.push(new Todo(1, 'Title', 'Description', false, new Date().toDateString())); 
+  }
+
+  addTodo(todo: Todo){
+    this.todos.push(todo);
+  }
 }
